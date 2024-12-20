e5fb57d2690d696d66288b9ac3ce2c052e611178
JuliusVerschoof <j.verschoof@student.maastrichtuniversity.nl>
2024-12-20 18:12:23 +0100

    changed it so that it works with swagger

--- live-demo-main/src/main/java/lsit/SwaggerConfig.java
+++ live-demo-main/src/main/java/lsit/SwaggerConfig.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.OAuthFlows;
 import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.Scopes;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,11 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Scopes scopes = new Scopes()
+                .addString("openid", "Access OpenID")
+                .addString("profile", "Access Profile")
+                .addString("email", "Access Email");
+
         return new OpenAPI()
                 .info(new Info().title("Your API").version("1.0"))
                 .addSecurityItem(new SecurityRequirement().addList("oauth2"))
@@ -24,6 +31,7 @@ public class SwaggerConfig {
                                 .flows(new OAuthFlows()
                                         .authorizationCode(new OAuthFlow()
                                                 .authorizationUrl("https://gitlab.com/oauth/authorize")
-                                                .tokenUrl("https://gitlab.com/oauth/token")))));
+                                                .tokenUrl("https://gitlab.com/oauth/token")
+                                                .scopes(scopes)))));
     }
 }
