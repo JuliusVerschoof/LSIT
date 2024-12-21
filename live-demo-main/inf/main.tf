39b5b829d3a44bffabc1b16b1086ebcb6d808d80
Sashank576 <csashank123@gmail.com>
2024-12-21 04:33:25 +0100

    Added automation files, no functionality

---
+++ live-demo-main/inf/main.tf
@@ -0,0 +1,77 @@
+resource "random_uuid" "name" {
+  
+}
+
+locals {
+  name=random_uuid.name.id
+}
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "google_compute_instance" "vm" {
+  name = "vm-${local.name}"
+
+  machine_type = "e2-small"
+
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20241115"
+      size = 20
+      type = "pd-standard"
+    }
+    mode = "READ_WRITE"
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "STANDARD"
+    }
+    network = "default"
+  }
+
+  tags = ["https-server", "http-server"]
+
+  zone = "us-central1-c"
+
+  metadata = {
+    ssh-keys = "user:${tls_private_key.key.public_key_openssh}"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "user"
+      host = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+      private_key = tls_private_key.key.private_key_openssh
+    }
+    inline = [
+      "curl -fsSL https://get.docker.com -o get-docker.sh",
+      "sudo sh get-docker.sh"
+    ]
+  }
+}
+
+resource "terraform_data" "runContainer" {
+    provisioner "remote-exec" {
+      connection {
+        type = "ssh"
+        user = "user"
+        host = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+        private_key = tls_private_key.key.private_key_openssh
+      }
+      inline = [
+        "sudo docker pull registry.gitlab.com/lsit-ken3239/live-demo:latest",
+        "sudo docker stop app",
+        "sudo docker rm app",
+        "sudo docker run -p80:8080 --name app -d registry.gitlab.com/lsit-ken3239/live-demo:latest"
+      ]
+  }
+}
+
+output "service_url" {
+  value = "http://${google_compute_instance.vm.network_interface[0].access_config[0].nat_ip}"
+}
---
+++ live-demo-main/inf/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">0.13"
+
+  backend "http"{
+    
+  }
+
+  required_providers {
+    google = {
+        source   = "hashicorp/google"
+        version  = "6.12.0"
+    }
+
+    random = {
+        source  = "hashicorp/random"
+        version = "~>3.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "lsit-ken3239"
+  region = "us-central1"
+}
